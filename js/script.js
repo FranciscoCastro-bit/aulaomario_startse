d478c72d9a866185d7bd715661d136245d4c15f5
Francisco Castro <rayatycastro@gmail.com>
2022-06-17 21:22:49 -0300

    escape

--- js/script.js
+++ js/script.js
@@ -10,13 +10,23 @@ const jump = () => {
 };
 
 const loopGame = setInterval(() => {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
 
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = "none";
+        pipe.style.left = `${pipePosition}px`;
 
-    
-})
+        mario.style.animation = "none";
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = "./images/mario-game-over.png";
+        mario.style.width = "75px";
+        mario.style.marginLeft = "45px";
+
+        clearInterval(loopGame)
+
+    }
+
+
+}, 10);
 
 document.addEventListener("keydown", jump);
